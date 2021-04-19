862f66d4b411d54f5fc6bfe965ff44e078b2882a
helmy <mohamedhelmy@bringy.com>
2021-04-19 02:14:04 +0200

    add database configuration to env

--- src/util/secrets.ts
+++ src/util/secrets.ts
@@ -5,3 +5,6 @@ export const ENVIRONMENT = process.env.NODE_ENV || "development";
 export const PORT = process.env.PORT;
 export const JWT_SECRET = process.env.JWT_SECRET_KEY;
 export const CORS_WHITELIST = process.env.CORS_WHITELIST.split(",");
+export const DB_NAME = process.env.DB_NAME;
+export const DB_USER = process.env.DB_USER;
+export const DB_PASSWORD = process.env.DB_PASSWORD;
