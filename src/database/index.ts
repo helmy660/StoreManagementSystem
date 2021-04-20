43f4b22b4ab872093aa5c5c78467cc239ecf7563
helmy <mohamedhelmy@bringy.com>
2021-04-20 03:09:00 +0200

    use ProductModel in Product class methods

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,2 +1,1 @@
-export * as MongoDB from "./Mongo";
 export { Product } from "./product";
--- src/database/product.ts
+++ src/database/product.ts
@@ -1,13 +1,24 @@
-import { MongoDB } from "../database";
+import mongoose from "mongoose";
+import { ProductModel } from "../models";
+import { IProduct } from "../interfaces";
+import { CustomError, ErrorTypes } from "../services";
 
 export class Product {
-  path: string;
-
-  constructor(sellerId: string) {
-    this.path = `sellers/${sellerId}/products`;
-  }
-
-  async add(data: any) {
-    return await MongoDB.add(this.path, data);
+  async add(data: IProduct) {
+    try {
+      return await ProductModel.create({
+        _id: new mongoose.Types.ObjectId(),
+        name: data.name,
+        picture: data.picture,
+        rate: data.rate,
+        price: data.price,
+        quantity: data.quantity,
+        offers: data.offers,
+        soldNumbers: data.soldNumbers,
+      });
+    } catch (error) {
+      console.log(error);
+      throw new CustomError(ErrorTypes.INVALID_REQUEST_BODY);
+    }
   }
 }
