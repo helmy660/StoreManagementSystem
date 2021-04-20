9feec06741fcda8eb40e9054b073da0dc06f36ba
helmy <mohamedhelmy@bringy.com>
2021-04-20 03:08:01 +0200

    update create function in product controller

---
+++ src/controllers/product/create.ts
@@ -0,0 +1,13 @@
+import { Response, Request, NextFunction } from "express";
+import { Product } from "../../database";
+
+export const create = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const product = new Product();
+    await product.add(req.body.data);
+    return res.status(200).json({ success: "true", message: "Created a new product sucessfully" });
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+};
