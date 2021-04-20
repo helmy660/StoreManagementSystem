cdcea3d9b267cd0d5be996b43e5ad599652a1d83
helmy <mohamedhelmy@bringy.com>
2021-04-20 02:48:13 +0200

    create ProductModel and ProductSchema

---
+++ src/models/product.ts
@@ -0,0 +1,14 @@
+import mongoose, { Schema } from "mongoose";
+import { IProduct } from "../interfaces";
+
+const ProductSchema: Schema = new Schema({
+  _id: Schema.Types.ObjectId,
+  picture: { type: String, required: true },
+  rate: { type: Number, required: true },
+  price: { type: Number, required: true },
+  quantity: { type: Number, required: true },
+  offers: { type: String, required: true },
+  soldNumbers: { type: Number, default: 0 },
+});
+
+export const ProductModel = mongoose.model<IProduct>("Product", ProductSchema);
