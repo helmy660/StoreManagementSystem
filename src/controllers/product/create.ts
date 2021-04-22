cceedbdd45fb14d39822cdd1d5716e46353dc3c0
helmy <mohamedhelmy@bringy.com>
2021-04-22 17:51:21 +0200

    read data from request body directly

--- src/controllers/product/create.ts
+++ src/controllers/product/create.ts
@@ -4,7 +4,7 @@ import { Product } from "../../database";
 export const create = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const product = new Product();
-    await product.add(req.body.data);
+    await product.add(req.body);
     return res.status(200).json({ success: "true", message: "Created a new product sucessfully" });
   } catch (error) {
     console.log(error);
