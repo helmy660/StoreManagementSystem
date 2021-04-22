f8bccc6d73cd75242384d7545bc5edb110f1764e
helmy <mohamedhelmy@bringy.com>
2021-04-22 18:32:06 +0200

    update validation middleware conditions for product route

--- src/routes/product.route.ts
+++ src/routes/product.route.ts
@@ -1,10 +1,28 @@
 import { Router } from "express";
 import { body, param, query } from "express-validator";
 import * as productController from "../controllers/product/create";
-import { logger, validator } from "../middlewares";
+import { logger, validator, auth } from "../middlewares";
 
 const router = Router();
 
-router.post("/", logger, [body("userId").exists({ checkNull: true }).isString()], validator, productController.create);
+router.post(
+  "/",
+  logger,
+  auth,
+  [
+    body("name").exists({ checkNull: true }).isString().isLength({ min: 3, max: 50 }),
+    body("picture")
+      .exists({ checkNull: true })
+      .isString()
+      .isURL()
+      .matches("[^\\s]+(.*?)\\.(jpg|jpeg|png|gif|JPG|JPEG|PNG|GIF)$"),
+    body("rate").exists({ checkNull: true }).isNumeric(),
+    body("price").exists({ checkNull: true }).isNumeric(),
+    body("quantity").exists({ checkNull: true }).isNumeric(),
+    body("soldNumbers").optional({ nullable: false }).isNumeric(),
+  ],
+  validator,
+  productController.create,
+);
 
 export default router;
