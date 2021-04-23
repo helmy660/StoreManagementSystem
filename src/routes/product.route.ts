ac50e74b0b2394b303d6b65087a986d87ac57cdc
helmy <mohamedhelmy@bringy.com>
2021-04-23 17:24:29 +0200

    handle new routes for products

--- src/routes/product.route.ts
+++ src/routes/product.route.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { body, param, query } from "express-validator";
-import * as productController from "../controllers/product/create";
-import { logger, validator, auth } from "../middlewares";
+import * as productController from "../controllers/product";
+import { logger, validator, auth, isSellerUser } from "../middlewares";
 
 const router = Router();
 
@@ -9,20 +9,68 @@ router.post(
   "/",
   logger,
   auth,
+  isSellerUser,
   [
-    body("name").exists({ checkNull: true }).isString().isLength({ min: 3, max: 50 }),
+    body("title").exists({ checkNull: true }).isString().isLength({ min: 3, max: 50 }),
+    body("description").exists({ checkNull: true }).isString().isLength({ min: 3, max: 300 }),
     body("picture")
       .exists({ checkNull: true })
       .isString()
       .isURL()
       .matches("[^\\s]+(.*?)\\.(jpg|jpeg|png|gif|JPG|JPEG|PNG|GIF)$"),
-    body("rate").exists({ checkNull: true }).isNumeric(),
-    body("price").exists({ checkNull: true }).isNumeric(),
-    body("quantity").exists({ checkNull: true }).isNumeric(),
-    body("soldNumbers").optional({ nullable: false }).isNumeric(),
+    body("rate").optional({ nullable: false }).isFloat({ gt: 0 }),
+    body("price").exists({ checkNull: true }).isFloat({ gt: 0 }),
+    body("quantity").exists({ checkNull: true }).isInt({ gt: 0 }),
+    body("categories").exists({ checkNull: true }).isArray({ min: 1 }).isMongoId(),
+    body("soldNumbers").optional({ nullable: false }).isInt({ gt: 0 }),
+    body("offer").optional({ nullable: false }).isFloat({ gt: 0 }),
   ],
   validator,
   productController.create,
 );
+
+router.put(
+  "/:productId",
+  logger,
+  auth,
+  isSellerUser,
+  [
+    body("title").optional({ nullable: false }).isString().isLength({ min: 3, max: 50 }),
+    body("description").optional({ nullable: false }).isString().isLength({ min: 3, max: 300 }),
+    body("picture")
+      .optional({ nullable: false })
+      .isString()
+      .isURL()
+      .matches("[^\\s]+(.*?)\\.(jpg|jpeg|png|gif|JPG|JPEG|PNG|GIF)$"),
+    body("rate").optional({ nullable: false }).isFloat({ gt: 0 }),
+    body("price").optional({ nullable: false }).isFloat({ gt: 0 }),
+    body("quantity").optional({ nullable: false }).isInt({ gt: 0 }),
+    body("categories").optional({ nullable: false }).isArray({ min: 1 }).isMongoId(),
+    body("soldNumbers").optional({ nullable: false }).isInt({ gt: 0 }),
+    body("offer").optional({ nullable: false }).isFloat({ gt: 0 }),
+  ],
+  validator,
+  productController.update,
+);
+
+router.get(
+  "/:productId",
+  logger,
+  auth,
+  [param("productId").exists({ checkNull: true }).isMongoId()],
+  validator,
+  productController.getById,
+);
+
+router.get(
+  "/category/:categoryId",
+  logger,
+  auth,
+  [param("categoryId").exists({ checkNull: true }).isMongoId()],
+  validator,
+  productController.getByCategory,
+);
+
+router.get("/", logger, auth, productController.getAll);
 
 export default router;
