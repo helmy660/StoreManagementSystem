a301d08c97d6242466707337ba4e80f5c4279450
helmy <mohamedhelmy@bringy.com>
2021-04-23 17:23:00 +0200

    update ProductSchema

--- src/models/product.ts
+++ src/models/product.ts
@@ -4,14 +4,14 @@ import { IProduct } from "../interfaces";
 const ProductSchema: Schema = new Schema({
   _id: Schema.Types.ObjectId,
   picture: { type: String, required: true },
-  rate: { type: Number, required: true },
+  rate: { type: Number, default: 0 },
   price: { type: Number, required: true },
   quantity: { type: Number, required: true },
-  offers: { type: String, default: null },
+  offer: { type: Number, default: null },
   soldNumbers: { type: Number, default: 0 },
   title: { type: String, required: true },
   description: { type: String, required: true },
-  categories: [{ type: String, required: true }],
+  categories: [{ type: Schema.Types.ObjectId, required: true }],
 });
 
 export const ProductModel = mongoose.model<IProduct>("Product", ProductSchema);
