b31982eb8aaf58cc437799f7907e5fe00e340838
helmy <mohamedhelmy@bringy.com>
2021-04-24 18:32:08 +0200

    create OrderSchema

--- src/models/index.ts
+++ src/models/index.ts
@@ -2,3 +2,4 @@ export { ProductModel } from "./product";
 export { UserModel } from "./user";
 export { CategoryModel } from "./category";
 export { CartModel } from "./cart";
+export { OrderModel } from "./order";
---
+++ src/models/order.ts
@@ -0,0 +1,21 @@
+import mongoose, { Schema } from "mongoose";
+import { OrderStatus, Currency } from "../enum";
+import { IOrder } from "../interfaces";
+
+const OrderSchema: Schema = new Schema(
+  {
+    _id: Schema.Types.ObjectId,
+    customerId: { type: String, required: true },
+    sellerId: { type: String, required: true },
+    status: { type: String, default: OrderStatus.CREATED },
+    totalItems: { type: Number, required: true },
+    totalPrice: { type: Number, required: true },
+    currency: { type: String, default: Currency.EGP },
+    products: [{ type: Object, required: true }],
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export const OrderModel = mongoose.model<IOrder>("Order", OrderSchema);
